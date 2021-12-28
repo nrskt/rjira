7cdbb42578fec4789577b6c6640430739d06339c
nrskt <chipuno0602@gmail.com>
2021-12-28 13:00:50 +0900

    feat(ports): impl error handling

--- ports/src/error.rs
+++ ports/src/error.rs
@@ -8,10 +8,24 @@ pub type PortsResult<T> = Result<T, PortsError>;
 pub enum PortsError {
     #[error("NotFound: {0:?}")]
     NotFound(String),
+    #[error("InternalError: {0}")]
+    InternalError(String),
 }
 
 impl PortsError {
     pub fn not_found(msg: impl Into<String>) -> Self {
         Self::NotFound(msg.into())
+    }
+}
+
+impl From<serde_yaml::Error> for PortsError {
+    fn from(err: serde_yaml::Error) -> Self {
+        Self::InternalError(err.to_string())
+    }
+}
+
+impl From<std::io::Error> for PortsError {
+    fn from(err: std::io::Error) -> Self {
+        Self::InternalError(err.to_string())
     }
 }
